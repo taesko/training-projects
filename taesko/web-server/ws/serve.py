8a6cf1f8fbfe6b9fbc9638f701103f4ba2395318
Antonio Todorov <taeskow@gmail.com>
2018-10-04 10:15:29 +0300

    Remove depreciated modules and functions from last v.

--- taesko/web-server/ws/serve.py
+++ taesko/web-server/ws/serve.py
@@ -19,46 +19,6 @@ assert_sys(os.path.isdir(STATIC_DIR),
            code='CONFIG_BAD_STATIC_DIR')
 
 
-def serve_file_depreciated(sock, route):
-    assert isinstance(sock, socket.socket)
-
-    not_found_response = HTTPResponse(
-        status_line=HTTPStatusLine(http_version='HTTP/1.1',
-                                   status_code=404,
-                                   reason_phrase=''),
-        headers=HTTPHeaders({'Content-Encoding': 'ascii'}),
-        body=None
-    )
-
-    if not route.startswith(STATIC_ROUTE):
-        not_found_response.send(sock)
-        return
-
-    rel_path = route[len(STATIC_ROUTE):]
-    abs_path = os.path.join(STATIC_DIR, rel_path)
-
-    try:
-        with open(abs_path, mode='r', encoding='utf-8') as f:
-            content = f.read()
-    except FileNotFoundError:
-        not_found_response.send(sock)
-        return
-
-    response = HTTPResponse(
-        status_line=HTTPStatusLine(http_version='HTTP/1.1',
-                                   status_code=200,
-                                   reason_phrase=''),
-        headers=HTTPHeaders({
-            'Content-Length': len(content),
-            'Content-Encoding': 'ascii'
-        }),
-        body=content
-    )
-
-    response.send(sock)
-    return
-
-
 def get_file(route):
     if not route.startswith(STATIC_ROUTE):
         return ws.err_responses.not_found()
@@ -88,7 +48,3 @@ def get_file(route):
         body=content
     )
 
-
-def serve_response_depreciated(sock, response):
-    error_log.debug('Sending back response %s', response)
-    return sock.sendall(bytes(response))
